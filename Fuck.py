48c843551d7b44aa35a5bdb3fbf32e795bef9a79
N00B HACK3R - TALHA <102674297+Talha-Niloy-Cyber@users.noreply.github.com>
2022-04-04 01:42:05 +0600

    Create Fuck.py

---
+++ Fuck.py
@@ -0,0 +1,24 @@
+print('''\n\033[1;31m
+
+ mm   m mmmmm  m       mmmm m     m        mmmmm  mmmm
+ #"m  #   #    #      m"  "m "m m"         #    # #   "m
+ # #m #   #    #      #    #  "#"          #mmmm" #    #
+ #  # #   #    #      #    #   #     """   #    # #    #
+ #   ## mm#mm  #mmmmm  #mm#    #           #mmmm" #mmm"
+  						v:0.0
+ NEW PHISHING  TOOL IS COMEING SOON:)
+ 
+ ->>>>DEVLOPER:NILOY(N00B HACK3R)
+ 
+->>>> CODER-TALHA ZUBAYER(N00B HACK3R)
+ 
+ 
+ ->>>>FACEBOOK-************
+ 
+->>>> PHONE-019**********
+ 
+ Note:(This Is paid Tool)
+ 
+★[FUCK OFF]
+
+\033[0m''')
